6ceea8c123d5ab6ff3b5795d3d89257b26d43f2a
Joel Alvarado Torres <joyalt77@gmail.com>
2015-09-30 04:45:16 -0500

    Mejoras en template

--- erp-web/src/main/java/ec/edu/ug/erp/web/template/jsf/component/HtmlMenuComponent.java
+++ erp-web/src/main/java/ec/edu/ug/erp/web/template/jsf/component/HtmlMenuComponent.java
@@ -45,11 +45,15 @@ public class HtmlMenuComponent extends UIComponentBase {
 	public static final String HTML_ATTR_CLASS="class";
 	public static final String HTML_ATTR_STYLE="style";
 	public static final String HTML_ATTR_HREF="href";
+	public static final String HTML_ATTR_SRC="src";
+	public static final String HTML_ATTR_PLACEHOLDER="placeholder";
+	public static final String PLACEHOLDER_BUSCAR="Buscar menu";	
+	public static final String SEARCH_ICON="fa fa-search";
 	
 	public static final String COL_MENU_CLASS="col-xs-2 col-sm-2";
 	public static final String MAIN_MENU_CLASS="nav main-menu";
 	public static final String SEARCH_MENU_CLASS="sidebar-search";
-	public static final String SEARCH_INPUT_CLASS="input-group custom-search-form";
+	public static final String SEARCH_INPUT_CLASS="mediumFont fontSize14 searchInput";
 	public static final String FORMCONTROL_CLASS="form-control";
 	public static final String INPUTGROUPBTN_CLASS="input-group-btn";
 	public static final String BTNDEFAULT_CLASS="btn btn-default";
@@ -63,7 +67,7 @@ public class HtmlMenuComponent extends UIComponentBase {
 	public static final String HREF_NONE="#";
 	public static final String STRING_EMPTY="";
 	public static final String ID_VALUE="sidebar-left";
-	public static final String DEFAULT_SUBMENU_ICON="fa fa-list";
+	public static final String DEFAULT_SUBMENU_ICON="fa fa-angle-right";
 	public static final String DEFAULT_MENUITEM_ICON="fa fa-angle-double-right";
 	public static final String STYLE_HIDDEN="display:none";
 
@@ -92,22 +96,13 @@ public class HtmlMenuComponent extends UIComponentBase {
 		
 		writer.startElement(HTML_LI, this);
 		writer.writeAttribute(HTML_ATTR_CLASS, SEARCH_MENU_CLASS, null);
-		writer.startElement(HTML_DIV, this);
-		writer.writeAttribute(HTML_ATTR_CLASS, SEARCH_INPUT_CLASS, null);
-		writer.startElement(HTML_INPUT, this);
-		writer.writeAttribute(HTML_ATTR_CLASS, FORMCONTROL_CLASS, null);
-		writer.endElement(HTML_INPUT);
-		writer.startElement(HTML_SPAN, this);
-		writer.writeAttribute(HTML_ATTR_CLASS, INPUTGROUPBTN_CLASS, null);
-		writer.startElement(HTML_BUTTON, this);
-		writer.writeAttribute(HTML_ATTR_CLASS, BTNDEFAULT_CLASS, null);
-		writer.writeAttribute(HTML_ATTR_TYPE, HTML_BUTTON, null);
 		writer.startElement(HTML_I, this);
-		writer.writeAttribute(HTML_ATTR_CLASS, FA_SEARCH_CLASS, null);
-		writer.endElement(HTML_I);
-		writer.endElement(HTML_BUTTON);
-		writer.endElement(HTML_SPAN);
-		writer.endElement(HTML_DIV);		
+		writer.writeAttribute(HTML_ATTR_CLASS, SEARCH_ICON, null);
+		writer.endElement(HTML_I);	
+		writer.startElement(HTML_INPUT, this);
+		writer.writeAttribute(HTML_ATTR_CLASS, SEARCH_INPUT_CLASS, null);
+		writer.writeAttribute(HTML_ATTR_PLACEHOLDER, PLACEHOLDER_BUSCAR, null);
+		writer.endElement(HTML_INPUT);
 		writer.endElement(HTML_LI);		
 		
 		
